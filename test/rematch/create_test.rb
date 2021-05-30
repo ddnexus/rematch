ef67f74c84021c89d15c0bcad234fb4a7d8d283e
Domizio Demichelis <dd.nexus@gmail.com>
2021-05-30 12:51:16 +0700

    fix for test depending on the previous

--- test/rematch/create_test.rb
+++ test/rematch/create_test.rb
@@ -4,15 +4,13 @@ require_relative '../test_helper'
 
 describe 'rematch/create' do
   store_file = "#{__FILE__}#{Rematch::EXT}"
-  it 'creates the store' do
+  it 'creates the store and the entry' do
     File.delete(store_file) if File.file?(store_file)
     _('a new value').must_rematch
     _(store_file).path_must_exist
-  end
-  it 'creates an entry' do
     _(File.read(store_file)).must_equal <<~STORE
       ---
-      "[1] rematch/create#test_0001_creates the store": a new value
+      "[1] rematch/create#test_0001_creates the store and the entry": a new value
     STORE
   end
 end
