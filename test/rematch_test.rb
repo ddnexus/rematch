6b45c1de770faab4149332b13a1a62d4b2792b47
Domizio Demichelis <dd.nexus@gmail.dom>
2021-10-28 14:40:40 +0700

    Added release-gem action

--- test/rematch_test.rb
+++ test/rematch_test.rb
@@ -3,10 +3,16 @@
 require_relative 'test_helper'
 
 describe 'rematch' do
-  it 'has version' do
-    _(Rematch::VERSION).wont_be_nil
+  describe 'version match' do
+    it 'has version' do
+      _(Rematch::VERSION).wont_be_nil
+    end
+    it 'defines the same version in CHANGELOG.md' do
+      _(File.read('CHANGELOG.md')).must_match "## Version #{Rematch::VERSION}"
+    end
+    it 'defines the same version in .github/.env' do
+      _(File.read('.github/.env')).must_match "VERSION=#{Rematch::VERSION}"
+    end
   end
-  it 'defines the same version in CHANGELOG.md' do
-    _(File.read('CHANGELOG.md')).must_match "## Version #{Rematch::VERSION}"
-  end
+
 end
